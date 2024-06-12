22af272d378d7fa87e2c07a2f4bc68f499bd4b84
Sandravijumon <sandravijumon15@gmail.com>
2024-06-12 21:14:18 +0530

    project created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddContacts from './components/AddContacts';
+import Search from './components/Search';
 
 function App() {
   return (
     <div>
       <AddContacts/>
+      <Search/>
     </div>
   );
 }
