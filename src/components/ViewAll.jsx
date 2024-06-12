e9007c65404c33167068bbfa2326a1388f7ed9dc
Sandravijumon <sandravijumon15@gmail.com>
2024-06-12 22:20:33 +0530

    project created

--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Navbar from './Navbar'
 
 const ViewAll = () => {
     const [data,changeData]=useState(
@@ -13,6 +14,7 @@ const ViewAll = () => {
     )
   return (
     <div>
+        <Navbar/>
         <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
